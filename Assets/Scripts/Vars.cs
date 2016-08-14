4b36df75f5d179f8dd1d074e9b993cc4ad05e5f2
Tigrou Ind <tigrou.ind@gmail.com>
2016-08-15 01:16:38 +0200

    cvars / vars refactoring

--- Assets/Scripts/Vars.cs
+++ Assets/Scripts/Vars.cs
@@ -3,158 +3,161 @@ using UnityEngine;
 
 public class Vars : MonoBehaviour
 {
-    public MenuStyle Style;
+	public MenuStyle Style;
 
-    private bool pauseVarsTracking;
+	private bool pauseVarsTracking;
 
-    private byte[] varsMemory = new byte[207*2];
-    private byte[] oldVarsMemory = new byte[207*2];
-    private float[] varsMemoryTime = new float[207];
-    private bool[] varsDifference = new bool[207];
-    private byte[] varsMemoryPattern = new byte[] { 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2E, 0x00, 0x2F, 0x00, 0x00, 0x00, 0x00 }; 
-    private long varsMemoryAddress = -1;
+	private byte[] memory = new byte[512];
+	private Var[] vars = new Var[207];
+	private Var[] cvars = new Var[44];
 
-    private byte[] cvarsMemory = new byte[44*2];
-    private byte[] oldCVarsMemory = new byte[44*2];
-    private float[] cvarsMemoryTime = new float[44];
-    private bool[] cvarsDifference = new bool[44];
-    private byte[] cvarsMemoryPattern = new byte[] { 0x31, 0x00, 0x0E, 0x01, 0xBC, 0x02, 0x12, 0x00, 0x06, 0x00, 0x13, 0x00, 0x14, 0x00, 0x01 }; 
-    private long cvarsMemoryAddress = -1;
+	private byte[] varsMemoryPattern = new byte[] { 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2E, 0x00, 0x2F, 0x00, 0x00, 0x00, 0x00 };
+	private long varsMemoryAddress = -1;
 
-    private short ReadShort(byte a, byte b)
-    {
-        unchecked
-        {
-            return (short)(a | b << 8);
-        }
-    }
+	private byte[] cvarsMemoryPattern = new byte[] { 0x31, 0x00, 0x0E, 0x01, 0xBC, 0x02, 0x12, 0x00, 0x06, 0x00, 0x13, 0x00, 0x14, 0x00, 0x01 };
+	private long cvarsMemoryAddress = -1;
 
-    void Update()
-    {
-        ProcessMemoryReader processReader = GetComponent<DosBox>().ProcessReader;
-        if (processReader != null)
-        {
-            if (!pauseVarsTracking)
-            {
-                if (varsMemoryAddress != -1)
-                {
-                    processReader.Read(varsMemory, varsMemoryAddress, varsMemory.Length);
-                    CheckDifferences(varsMemory, oldVarsMemory, varsMemoryTime, varsDifference, 207);
-                }
+	private short ReadShort(byte a, byte b)
+	{
+		unchecked
+		{
+			return (short)(a | b << 8);
+		}
+	}
 
-                if (cvarsMemoryAddress != -1)
-                {
-                    processReader.Read(cvarsMemory, cvarsMemoryAddress, cvarsMemory.Length);
-                    CheckDifferences(cvarsMemory, oldCVarsMemory, cvarsMemoryTime, cvarsDifference, 44);
-                }
-            }
-        }
+	void Update()
+	{
+		ProcessMemoryReader processReader = GetComponent<DosBox>().ProcessReader;
+		if (processReader != null)
+		{
+			if (!pauseVarsTracking)
+			{
+				if (varsMemoryAddress != -1)
+				{
+					processReader.Read(memory, varsMemoryAddress, 207 * 2);
+					CheckDifferences(memory, vars);
+				}
 
-        //freeze vars tracking
-        if (Input.GetMouseButtonDown(0))
-        {
-            pauseVarsTracking = !pauseVarsTracking;
-        }
+				if (cvarsMemoryAddress != -1)
+				{
+					processReader.Read(memory, cvarsMemoryAddress, 44 * 2);
+					CheckDifferences(memory, cvars);
+				}
+			}
+		}
 
-        //hide table
-        if (Input.GetMouseButtonDown(1))
-        {
-            pauseVarsTracking = false;
-            GetComponent<Vars>().enabled = false;
-        }
-    }
+		//freeze vars tracking
+		if (Input.GetMouseButtonDown(0))
+		{
+			pauseVarsTracking = !pauseVarsTracking;
+		}
 
-    void OnGUI()
-    {
-        GUIStyle panel = new GUIStyle(Style.Panel);
-        panel.normal.background = Style.BlackTexture;
-        Rect areaA = new Rect(0, 0, Screen.width, Screen.height * 22.0f/28.0f);
-        Rect areaB = new Rect(0, Screen.height * 22.0f/28.0f, Screen.width, Screen.height * 6.0f/28.0f);
+		//hide table
+		if (Input.GetMouseButtonDown(1))
+		{
+			pauseVarsTracking = false;
+			GetComponent<Vars>().enabled = false;
+		}
+	}
 
-        GUILayout.BeginArea(areaA, panel);
-        DisplayTable(areaA, 10, 21, varsMemory, varsDifference, "VARS");
-        GUILayout.EndArea();
+	void OnGUI()
+	{
+		GUIStyle panel = new GUIStyle(Style.Panel);
+		panel.normal.background = Style.BlackTexture;
+		Rect areaA = new Rect(0, 0, Screen.width, Screen.height * 22.0f / 28.0f);
+		Rect areaB = new Rect(0, Screen.height * 22.0f / 28.0f, Screen.width, Screen.height * 6.0f / 28.0f);
 
-        GUILayout.BeginArea(areaB, panel);
-        DisplayTable(areaB, 10, 5, cvarsMemory, cvarsDifference, "CVARS");
-        GUILayout.EndArea();
-    }
+		GUILayout.BeginArea(areaA, panel);
+		DisplayTable(areaA, 10, 21, vars, "VARS");
+		GUILayout.EndArea();
 
-    void CheckDifferences(byte[] values, byte[] oldvalues, float[] time, bool[] difference, int count)
-    {
-        float currenttime = Time.time;
-        for(int i = 0 ; i < count ; i++)
-        {
-            int value = ReadShort(values[i * 2 + 0], values[i * 2 + 1]);
-            int oldValue = ReadShort(oldvalues[i * 2 + 0], oldvalues[i * 2 + 1]);
-            if (value != oldValue)
-            {
-                time[i] = currenttime;
-            }
+		GUILayout.BeginArea(areaB, panel);
+		DisplayTable(areaB, 10, 5, cvars, "CVARS");
+		GUILayout.EndArea();
+	}
 
-            difference[i] = (currenttime - time[i]) < 5.0f;
+	void CheckDifferences(byte[] memory, Var[] data)
+	{
+		float currenttime = Time.time;
+		for (int i = 0; i < data.Length; i++)
+		{
+			int value = ReadShort(memory[i * 2 + 0], memory[i * 2 + 1]);
+			int oldValue = data[i].value;
+			data[i].value = value;
 
-            oldvalues[i * 2 + 0] = values[i * 2 + 0];
-            oldvalues[i * 2 + 1] = values[i * 2 + 1];
-        }
-    }
+			if (value != oldValue)
+			{
+				data[i].time = currenttime;
+			}
 
-    void DisplayTable(Rect area, int columns, int rows, byte[] values, bool[] difference, string title)
-    {
-        //setup style
-        GUIStyle labelStyle = new GUIStyle(Style.Label);
-        labelStyle.fixedWidth = area.width/(columns + 1);
-        labelStyle.fixedHeight = area.height/((float)(rows + 1));
-        labelStyle.alignment = TextAnchor.MiddleCenter;
+			data[i].difference = (currenttime - data[i].time) < 5.0f;
+			data[i].oldValue = value;
+		}
+	}
 
-        GUIStyle headerStyle = new GUIStyle(labelStyle);
-        headerStyle.normal.textColor = Color.black;
-        headerStyle.normal.background = pauseVarsTracking ? Style.RedTexture : Style.GreenTexture;
+	void DisplayTable(Rect area, int columns, int rows, Var[] vars, string title)
+	{
+		//setup style
+		GUIStyle labelStyle = new GUIStyle(Style.Label);
+		labelStyle.fixedWidth = area.width / (columns + 1);
+		labelStyle.fixedHeight = area.height / ((float)(rows + 1));
+		labelStyle.alignment = TextAnchor.MiddleCenter;
 
-        //header
-        GUILayout.BeginHorizontal();
-        GUILayout.Label(title, headerStyle);
-        for (int i = 0 ; i < columns ; i++)
-        {
-            GUILayout.Label(i.ToString(), headerStyle);
-        }
-        GUILayout.EndHorizontal();
+		GUIStyle headerStyle = new GUIStyle(labelStyle);
+		headerStyle.normal.textColor = Color.black;
+		headerStyle.normal.background = pauseVarsTracking ? Style.RedTexture : Style.GreenTexture;
 
-        //body
-        int count = 0;
-        for (int i = 0 ; i < rows ; i++)
-        {
-            GUILayout.BeginHorizontal();
-            headerStyle.alignment = TextAnchor.MiddleRight;
-            GUILayout.Label(i.ToString(), headerStyle);
+		//header
+		GUILayout.BeginHorizontal();
+		GUILayout.Label(title, headerStyle);
+		for (int i = 0; i < columns; i++)
+		{
+			GUILayout.Label(i.ToString(), headerStyle);
+		}
+		GUILayout.EndHorizontal();
 
-            for (int j = 0; j < columns; j++)
-            {
-                string stringValue = string.Empty;
-                if (count < values.Length / 2)
-                {
-                    int value = ReadShort(values[count * 2 + 0], values[count * 2 + 1]);
-                    bool different = difference[count];
+		//body
+		int count = 0;
+		for (int i = 0; i < rows; i++)
+		{
+			GUILayout.BeginHorizontal();
+			headerStyle.alignment = TextAnchor.MiddleRight;
+			GUILayout.Label(i.ToString(), headerStyle);
 
-                    if (value != 0 || different)
-                        stringValue = value.ToString();
+			for (int j = 0; j < columns; j++)
+			{
+				string stringValue = string.Empty;
+				if (count < vars.Length)
+				{
+					int value = vars[count].value;
+					bool different = vars[count].difference;
 
-                    //highlight recently changed vars
-                    labelStyle.normal.background = different ? Style.RedTexture : null;
-                }
+					if (value != 0 || different)
+						stringValue = value.ToString();
 
-                count++;
-                GUILayout.Label(stringValue, labelStyle);
-            }
-            GUILayout.EndHorizontal();
-        }
-    }
+					//highlight recently changed vars
+					labelStyle.normal.background = different ? Style.RedTexture : null;
+				}
 
-    public void SearchForPatterns(ProcessMemoryReader reader)
-    {
-        varsMemoryAddress = reader.SearchForBytePattern(varsMemoryPattern);
-        cvarsMemoryAddress = reader.SearchForBytePattern(cvarsMemoryPattern);
-    }
+				count++;
+				GUILayout.Label(stringValue, labelStyle);
+			}
+			GUILayout.EndHorizontal();
+		}
+	}
 
+	public void SearchForPatterns(ProcessMemoryReader reader)
+	{
+		varsMemoryAddress = reader.SearchForBytePattern(varsMemoryPattern);
+		cvarsMemoryAddress = reader.SearchForBytePattern(cvarsMemoryPattern);
+	}
 
+	public struct Var
+	{
+		public int value;
+		public int oldValue;
+		public int state;
+		public float time;
+		public bool difference;
+	}
 }
