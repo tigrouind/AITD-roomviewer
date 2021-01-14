6729882c35366810a3627e9f7d1ae0c034d1deb7
Tigrou Ind <tigrou.ind@gmail.com>
2021-01-14 23:46:26 +0100

    Refactor

--- Assets/Scripts/Room/CameraHelper.cs
+++ Assets/Scripts/Room/CameraHelper.cs
@@ -6,14 +6,11 @@ public class CameraHelper : MonoBehaviour
 {
 	public Mesh CubeMesh;
 
-	private readonly List<Vector3> vertices = new List<Vector3>();
-	private readonly List<int> indices = new List<int>();
-
 	private readonly Vector3[] quad = new Vector3[]
 	{
 		new Vector3( 1.0f, 1.0f, 1.0f),
-		new Vector3( 1.0f,-1.0f, 1.0f),
-		new Vector3(-1.0f,-1.0f, 1.0f),
+		new Vector3( 1.0f, 1.0f,-1.0f),
+		new Vector3(-1.0f, 1.0f,-1.0f),
 		new Vector3(-1.0f, 1.0f, 1.0f)
 	};
 
@@ -22,40 +19,37 @@ public class CameraHelper : MonoBehaviour
 		0, 1, 1, 2, 2, 3, 3, 0,         //far plane
 		4, 5, 5, 6, 6, 7, 7, 4,         //near plane
 		4, 0, 5, 1, 6, 2, 7, 3,         //edges between near and far
-		9, 10, 11, 8,                   //parallel lines
+		9, 10, 11, 8,                   //parallel lines behind
 		12, 13, 13, 14, 14, 15, 15, 12  //2D border
 	};
 
 	public void SetupTransform(Transform transform, Vector3 cameraPosition)
 	{
-		var pos = new Vector3(cameraPosition.x, cameraPosition.y, -cameraPosition.z) / 100.0f;
-		transform.position = pos;
+		transform.position = new Vector3(cameraPosition.x, cameraPosition.y, -cameraPosition.z) / 100.0f;
 	}
 
 	public Mesh CreateMesh(Vector3 cameraRotation, Vector3 cameraFocal)
 	{
-		var rot = cameraRotation / 1024.0f * 360.0f;
-		var qrot = Quaternion.Euler(rot.x, rot.y, rot.z);
+		var qrot = Quaternion.Euler(cameraRotation / 1024.0f * 360.0f);
 		var offset = qrot * new Vector3(0.0f, 0.0f, cameraFocal.x) / 1000.0f;
+		var focal = new Vector3(160.0f / cameraFocal.y, 1.0f, 100.0f / cameraFocal.z);
 
-		Matrix4x4 matrixA = Matrix4x4.TRS(-offset, qrot, Vector3.one);
-		Matrix4x4 matrixB = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(-90.0f, 0.0f, 0.0f), new Vector3(32.767f, 32.767f, 0.0f));
+		Matrix4x4 matrix = Matrix4x4.TRS(-offset, qrot * Quaternion.Euler(90.0f, 0.0f, 0.0f), Vector3.one);
 
-		var foc = new Vector3(160.0f / cameraFocal.y, 100.0f / cameraFocal.z, 1.0f);
-
-		var pts = quad.Select(x => Vector3.Scale(x, foc) * 32.767f)
-		  .Concat(quad.Select(x => Vector3.Scale(x, foc) * 0.4f))
-		  .Concat(quad.Select(x => Vector3.Scale(x, new Vector3(-500.0f, 2.0f, 0.0f))))
-		  .Select(x => matrixA.MultiplyPoint3x4(x))
-		  .Concat(quad.Select(x => matrixB.MultiplyPoint3x4(x)))
+		var pts = quad.Select(x => x * 32.768f)
+		  .Concat(quad.Select(x => x * 0.4f))
+		  .Select(x => Vector3.Scale(x, focal))
+		  .Concat(quad.Select(x => Vector3.Scale(x, new Vector3(-500.0f, 0.0f, 2.0f))))
+		  .Select(x => matrix.MultiplyPoint3x4(x))
+		  .Concat(quad.Select(x => Vector3.Scale(x, new Vector3(32.768f, 0.0f, 32.768f))))
 		  .ToArray();
 
-		vertices.Clear();
-		indices.Clear();
+		var vertices = new List<Vector3>();
+		var indices = new List<int>();
 
 		for (int i = 0; i < lines.Length; i += 2)
 		{
-			AddLine(pts[lines[i]], pts[lines[i + 1]]);
+			AddLine(pts[lines[i]], pts[lines[i + 1]], indices, vertices);
 		}
 
 		Mesh mesh = new Mesh();
@@ -68,16 +62,17 @@ public class CameraHelper : MonoBehaviour
 		return mesh;
 	}
 
-	private void AddLine(Vector3 a, Vector3 b)
+	private void AddLine(Vector3 a, Vector3 b, List<int> indices, List<Vector3> vertices)
 	{
 		const float linesize = 0.04f;
 		Vector3 directionVector = a - b;
 		Vector3 middle = (a + b) / 2.0f;
 		Quaternion rotation = Quaternion.LookRotation(directionVector);
 		Vector3 scale = new Vector3(linesize, linesize, directionVector.magnitude);
+		Matrix4x4 matrix = Matrix4x4.TRS(middle, rotation, scale);
 
 		indices.AddRange(CubeMesh.triangles.Select(x => x + vertices.Count));
-		vertices.AddRange(CubeMesh.vertices.Select(x => rotation * Vector3.Scale(x, scale) + middle));
+		vertices.AddRange(CubeMesh.vertices.Select(x => matrix.MultiplyPoint3x4(x)));
 	}
 
 	public Mesh SetupBorder()
@@ -85,26 +80,17 @@ public class CameraHelper : MonoBehaviour
 		var triangles = new int[]
 		{
 			0, 1, 5, 0, 5, 4,
-			6, 3, 2, 6, 7, 3,
-			0, 4, 2, 4, 6, 2,
-			1, 3, 7, 5, 1, 7
+			3, 6, 2, 7, 6, 3,
+			0, 4, 7, 7, 3, 0,
+			6, 5, 1, 6, 1, 2
 		};
 
 		const float outerSize = 32.768f;
 		const float innerSize = outerSize + 0.3f;
 
-		var vertices = new Vector3[]
-		{
-			new Vector3(-innerSize, 0f,  innerSize),
-			new Vector3( innerSize, 0f,  innerSize),
-			new Vector3(-innerSize, 0f, -innerSize),
-			new Vector3( innerSize, 0f, -innerSize),
-
-			new Vector3(-outerSize, 0f,  outerSize),
-			new Vector3( outerSize, 0f,  outerSize),
-			new Vector3(-outerSize, 0f, -outerSize),
-			new Vector3( outerSize, 0f, -outerSize)
-		};
+		var vertices = quad.Select(x => Vector3.Scale(x, new Vector3(innerSize, 0.0f, innerSize)))
+			.Concat(quad.Select(x => Vector3.Scale(x, new Vector3(outerSize, 0.0f, outerSize))))
+			.ToArray();
 
 		Mesh mesh = new Mesh();
 		mesh.vertices = vertices;
