tigrouind/AITD-roomviewer

commit fa55ac2eddd6049e20a521c2cd201a94482d4094

Add Vector3Int and Vector2Int types

Assets/Scripts/Model/ModelLoader.cs
@@ -31,7 +31,7 @@ public class ModelLoader : MonoBehaviour
 	private List<Vector3> initialBonesPosition;
 	private int modelFlags;
 	private int previousFrame;
-	private Vector3 frameDistance;
+	private Vector3Int frameDistance;
 
 	private int paletteIndex;
 	public Texture2D[] PaletteTexture;
@@ -44,8 +44,8 @@ public class ModelLoader : MonoBehaviour
 	private List<Vector3> allVertices;
 	private List<Vector2> uv;
 	private List<Vector2> uvDepth;
-	private Vector3 boundingLower;
-	private Vector3 boundingUpper;
+	private Vector3Int boundingLower;
+	private Vector3Int boundingUpper;
 
 	private Vector2 cameraRotation;
 	private Vector2 cameraPosition;
@@ -97,8 +97,8 @@ public class ModelLoader : MonoBehaviour
 
 		//bounding box
 		buffer.ReadBoundingBox(i + 2, out boundingLower, out boundingUpper);
-		BoundingBox.transform.localScale = (boundingUpper - boundingLower) / 1000.0f;
-		var pos = (boundingUpper + boundingLower) / 2000.0f;
+		BoundingBox.transform.localScale = (Vector3)(boundingUpper - boundingLower) / 1000.0f;
+		Vector3Int pos = (boundingUpper + boundingLower) / 2000;
 		BoundingBox.transform.localPosition = new Vector3(pos.x, -pos.y, pos.z);
 
 		i += 0xE;
@@ -669,7 +669,7 @@ public class ModelLoader : MonoBehaviour
 			{
 				Bone b = new Bone();
 				b.Type = buffer.ReadShort(i + 0);
-				Vector3 boneTransform = buffer.ReadVector(i + 2);
+				Vector3Int boneTransform = buffer.ReadVector(i + 2);
 
 				switch(b.Type)
 				{
@@ -793,7 +793,7 @@ public class ModelLoader : MonoBehaviour
 		{
 			var scale = new Vector3(1.0f, -1.0f, 1.0f) / 1000.0f;
 			var offset = transform.rotation *
-				Vector3.Scale(nextFrame.Offset * framePosition + frameDistance, scale);
+				Vector3.Scale((Vector3)nextFrame.Offset * framePosition + frameDistance, scale);
 			bones[0].transform.position += offset;
 			BoundingBox.transform.position = offset;
 		}
@@ -1235,7 +1235,7 @@ public class ModelLoader : MonoBehaviour
 		}
 
 		LeftText.text = stringBuilder.ToString();
-		frameDistance = Vector3.zero;
+		frameDistance = Vector3Int.zero;
 	}
 
 	public void ToggleAnimationMenuItems(bool enabled)

Assets/Scripts/DosBox/DosBox.cs
@@ -179,7 +179,7 @@ public class DosBox : MonoBehaviour
 					}
 					else
 					{
-						box.Mod = Vector3.zero;
+						box.Mod = Vector3Int.zero;
 					}
 
 					box.OldAngle = memory.ReadShort(k + 106);
@@ -248,7 +248,7 @@ public class DosBox : MonoBehaviour
 					if (roomObject != null)
 					{
 						//local to global position
-						Vector3 boxPosition = box.BoundingPos / 1000.0f;
+						Vector3 boxPosition = (Vector3)box.BoundingPos / 1000.0f;
 						boxPosition = new Vector3(boxPosition.x, -boxPosition.y, boxPosition.z) + roomObject.localPosition;
 
 						if (box.transform.position != boxPosition)
@@ -411,7 +411,7 @@ public class DosBox : MonoBehaviour
 				box.BoxHotPoint = hotPoint;
 			}
 
-			Vector3 finalPos = (box.HotPosition + box.LocalPosition + box.Mod) / 1000.0f;
+			Vector3 finalPos = (Vector3)(box.HotPosition + box.LocalPosition + box.Mod) / 1000.0f;
 			finalPos = new Vector3(finalPos.x, -finalPos.y, finalPos.z) + roomPosition;
 			hotPoint.transform.position = finalPos;
 
@@ -431,12 +431,12 @@ public class DosBox : MonoBehaviour
 		if (currentRoom != null)
 		{
 			Box worldPos = box.BoxWorldPos;
-			Vector3 boundingPos = box.WorldPosition + box.Mod + (currentRoom.localPosition - roomPosition) * 1000.0f;
+			Vector3Int boundingPos = box.WorldPosition + box.Mod + Vector3Int.FloorToInt((currentRoom.localPosition - roomPosition) * 1000.0f);
 
 			//worldpos unsync
 			if (isPlayer &&
-				  (Mathf.RoundToInt(boundingPos.x) != Mathf.RoundToInt(box.BoundingPos.x) ||
-				   Mathf.RoundToInt(boundingPos.z) != Mathf.RoundToInt(box.BoundingPos.z)))
+				  (boundingPos.x != box.BoundingPos.x ||
+				   boundingPos.z != box.BoundingPos.z))
 			{
 				if (worldPos == null)
 				{
@@ -447,7 +447,7 @@ public class DosBox : MonoBehaviour
 					box.BoxWorldPos = worldPos;
 				}
 
-				Vector3 finalPos = (box.WorldPosition + box.Mod) / 1000.0f;
+				Vector3 finalPos = (Vector3)(box.WorldPosition + box.Mod) / 1000.0f;
 				float height = -box.BoundingPos.y / 1000.0f;
 				finalPos = new Vector3(finalPos.x, height + 0.001f, finalPos.z) + currentRoom.localPosition;
 				worldPos.transform.position = finalPos;
@@ -489,8 +489,8 @@ public class DosBox : MonoBehaviour
 				BoxInfo.Append("Total delay", "{0:D2}:{1:D2}:{2:D2}.{3:D3} ", totalDelayTS.Hours, totalDelayTS.Minutes, totalDelayTS.Seconds, totalDelayTS.Milliseconds);
 			}
 
-			Vector3 mousePosition = GetMousePosition(linkroom, linkfloor);
-			BoxInfo.Append("Cursor position", "{0} {1}", Mathf.Clamp((int)(mousePosition.x), -32768, 32767), Mathf.Clamp((int)(mousePosition.z), -32768, 32767));
+			Vector3Int mousePosition = GetMousePosition(linkroom, linkfloor);
+			BoxInfo.Append("Cursor position", "{0} {1}", Math.Min(Math.Max(mousePosition.x, -32768), 32767), Math.Min(Math.Max(mousePosition.z, -32768), 32767));
 			if(Player != null) BoxInfo.Append("Last offset/dist", "{0}; {1}", Player.LastOffset, Mathf.RoundToInt(Player.LastDistance));
 
 			if (ShowAITD1Vars)
@@ -612,17 +612,17 @@ public class DosBox : MonoBehaviour
 		}
 	}
 
-	void FixBoundingWrap(ref float a, ref float b)
+	void FixBoundingWrap(ref int a, ref int b)
 	{
 		if(a > b)
 		{
 			if(a < -b)
 			{
-				b += 65536.0f;
+				b += 65536;
 			}
 			else
 			{
-				a -= 65536.0f;
+				a -= 65536;
 			}
 		}
 	}
@@ -806,7 +806,7 @@ public class DosBox : MonoBehaviour
 		return actorsAddress + index * actorStructSize[dosBoxPattern];
 	}
 
-	public Vector3 GetMousePosition(int room, int floor)
+	public Vector3Int GetMousePosition(int room, int floor)
 	{
 		Vector3 cameraHeight = new Vector3(0.0f, 0.0f, Camera.main.transform.position.y);
 		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + cameraHeight);
@@ -815,7 +815,7 @@ public class DosBox : MonoBehaviour
 		{
 			mousePosition -= roomObject.position;
 		}
-		return mousePosition * 1000.0f;
+		return Vector3Int.FloorToInt(mousePosition * 1000.0f);
 	}
 
 	#endregion

Assets/Scripts/DosBox/WarpDialog.cs
@@ -83,32 +83,32 @@ public class WarpDialog : MonoBehaviour
 
 			if (Input.GetKey(KeyCode.Keypad3))
 			{
-				MoveActor(actor, new Vector3(0.0f, -1.0f, 0.0f));
+				MoveActor(actor, new Vector3Int(0, -1, 0));
 			}
 
 			if (Input.GetKey(KeyCode.Keypad1))
 			{
-				MoveActor(actor, new Vector3(0.0f, 1.0f, 0.0f));
+				MoveActor(actor, new Vector3Int(0, 1, 0));
 			}
 
 			if (Input.GetKey(KeyCode.Keypad4))
 			{
-				MoveActor(actor, new Vector3(-1.0f, 0.0f, 0.0f));
+				MoveActor(actor, new Vector3Int(-1, 0, 0));
 			}
 
 			if (Input.GetKey(KeyCode.Keypad6))
 			{
-				MoveActor(actor, new Vector3(1.0f, 0.0f, 0.0f));
+				MoveActor(actor, new Vector3Int(1, 0, 0));
 			}
 
 			if (Input.GetKey(KeyCode.Keypad2))
 			{
-				MoveActor(actor, new Vector3(0.0f, 0.0f, -1.0f));
+				MoveActor(actor, new Vector3Int(0, 0, -1));
 			}
 
 			if (Input.GetKey(KeyCode.Keypad8))
 			{
-				MoveActor(actor, new Vector3(0.0f, 0.0f, 1.0f));
+				MoveActor(actor, new Vector3Int(0, 0, 1));
 			}
 		}
 
@@ -138,8 +138,8 @@ public class WarpDialog : MonoBehaviour
 
 		if (actor != null)
 		{
-			Vector3 offset = GetComponent<DosBox>().GetMousePosition(actor.Room, actor.Floor) - (actor.LocalPosition + actor.Mod);
-			offset = new Vector3(Mathf.RoundToInt(offset.x), 0.0f, Mathf.RoundToInt(offset.z));
+			Vector3Int offset = GetComponent<DosBox>().GetMousePosition(actor.Room, actor.Floor) - (actor.LocalPosition + actor.Mod);
+			offset = new Vector3Int(offset.x, 0, offset.z);
 			MoveActor(actor, offset);
 		}
 	}
@@ -160,13 +160,13 @@ public class WarpDialog : MonoBehaviour
 	void SetPosition(Box actor)
 	{
 		//parse angle
-		Vector3 angleInt;
+		Vector3Int angleInt;
 		TryParseAngle(AngleX, AngleY, AngleZ, out angleInt, actor.Angles);
 		WriteActorAngle(actor, angleInt);
 		UpdateAngleInputField(actor);
 
 		//parse position
-		Vector3 lowerBound, upperBound, local, world;
+		Vector3Int lowerBound, upperBound, local, world;
 
 		if (!AdvancedMode.BoolValue)
 		{
@@ -174,21 +174,21 @@ public class WarpDialog : MonoBehaviour
 			local -= actor.Mod;
 
 			//apply offset to world/bound
-			Vector3 offset = local - actor.LocalPosition;
+			Vector3Int offset = local - actor.LocalPosition;
 			world = actor.WorldPosition + offset;
 			lowerBound = actor.BoundingLower + offset;
 			upperBound = actor.BoundingUpper + offset;
 		}
 		else
 		{
-			Vector3 boundingPos;
+			Vector3Int boundingPos;
 			TryParsePosition(BoundingPosX, BoundingPosY, BoundingPosZ, out boundingPos, actor.BoundingPos);
 			TryParsePosition(LocalPosX, LocalPosY, LocalPosZ, out local, actor.LocalPosition + actor.Mod);
 			TryParsePosition(worldPosX, worldPosY, worldPosZ, out world, actor.WorldPosition + actor.Mod);
 			local -= actor.Mod;
 			world -= actor.Mod;
 
-			Vector3 offset = boundingPos - actor.BoundingPos;
+			Vector3Int offset = boundingPos - actor.BoundingPos;
 			lowerBound = actor.BoundingLower + offset;
 			upperBound = actor.BoundingUpper + offset;
 		}
@@ -218,18 +218,18 @@ public class WarpDialog : MonoBehaviour
 		int angle = (int)actor.Angles.y + offset;
 		angle = (angle + 1024) % 1024;
 
-		WriteActorAngle(actor, new Vector3(actor.Angles.x, angle, actor.Angles.z));
+		WriteActorAngle(actor, new Vector3Int(actor.Angles.x, angle, actor.Angles.z));
 		UpdateAngleInputField(actor);
 		timer.Restart();
 	}
 
 	//input keys or warp
-	void MoveActor(Box actor, Vector3 offset)
+	void MoveActor(Box actor, Vector3Int offset)
 	{
-		Vector3 local = actor.LocalPosition + offset;
-		Vector3 world = actor.WorldPosition + offset;
-		Vector3 boundLow = actor.BoundingLower + offset;
-		Vector3 boundUpper = actor.BoundingUpper + offset;
+		Vector3Int local = actor.LocalPosition + offset;
+		Vector3Int world = actor.WorldPosition + offset;
+		Vector3Int boundLow = actor.BoundingLower + offset;
+		Vector3Int boundUpper = actor.BoundingUpper + offset;
 
 		WriteActorPosition(actor, boundLow, boundUpper, local, world);
 		UpdatePositionInputFields(actor);
@@ -256,14 +256,14 @@ public class WarpDialog : MonoBehaviour
 		AngleZ.text = (actor.Angles.z * 360.0f / 1024.0f).ToString("N1");
 	}
 
-	void TryParseAngle(InputField angleX, InputField angleY, InputField angleZ, out Vector3 intValue, Vector3 defaultValue)
+	void TryParseAngle(InputField angleX, InputField angleY, InputField angleZ, out Vector3Int intValue, Vector3Int defaultValue)
 	{
 		int x, y, z;
 		TryParseAngle(angleX, out x, (int)defaultValue.x);
 		TryParseAngle(angleY, out y, (int)defaultValue.y);
 		TryParseAngle(angleZ, out z, (int)defaultValue.z);
 
-		intValue = new Vector3(x, y, z);
+		intValue = new Vector3Int(x, y, z);
 	}
 
 	void TryParseAngle(InputField inputField, out int intValue, int defaultValue)
@@ -280,14 +280,14 @@ public class WarpDialog : MonoBehaviour
 		}
 	}
 
-	void TryParsePosition(InputField posX, InputField posY, InputField posZ, out Vector3 intValue, Vector3 defaultValue)
+	void TryParsePosition(InputField posX, InputField posY, InputField posZ, out Vector3Int intValue, Vector3Int defaultValue)
 	{
 		int x, y, z;
 		TryParsePosition(posX, out x, (int)defaultValue.x);
 		TryParsePosition(posY, out y, (int)defaultValue.y);
 		TryParsePosition(posZ, out z, (int)defaultValue.z);
 
-		intValue = new Vector3(x, y, z);
+		intValue = new Vector3Int(x, y, z);
 	}
 
 	void TryParsePosition(InputField inputField, out int intValue, int defaultValue)
@@ -302,7 +302,7 @@ public class WarpDialog : MonoBehaviour
 		}
 	}
 
-	void WriteActorAngle(Box actor, Vector3 angle)
+	void WriteActorAngle(Box actor, Vector3Int angle)
 	{
 		ProcessMemoryReader processReader = GetComponent<DosBox>().ProcessReader;
 
@@ -314,7 +314,7 @@ public class WarpDialog : MonoBehaviour
 		actor.Angles = angle;
 	}
 
-	void WriteActorPosition(Box actor, Vector3 lowerBound, Vector3 upperBound, Vector3 localPosition, Vector3 worldPosition)
+	void WriteActorPosition(Box actor, Vector3Int lowerBound, Vector3Int upperBound, Vector3Int localPosition, Vector3Int worldPosition)
 	{
 		ProcessMemoryReader processReader = GetComponent<DosBox>().ProcessReader;
 

Assets/Scripts/Model/Frame.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Frame
 {
 	public float Time;
-	public Vector3 Offset;
+	public Vector3Int Offset;
 	public List<Bone> Bones;
 }
 

Assets/Scripts/Room/Box.cs
@@ -41,18 +41,18 @@ public class Box : MonoBehaviour
 	public int HitForce;
 	public int KeyFrameTime;
 	public int KeyFrameLength;
-	public Vector3 Mod;
-	public Vector3 LocalPosition;
-	public Vector3 WorldPosition;
-	public Vector3 BoundingLower;
-	public Vector3 BoundingUpper;
-	public Vector2 Box2DLower;
-	public Vector2 Box2DUpper;
-	public Vector3 Angles;
+	public Vector3Int Mod;
+	public Vector3Int LocalPosition;
+	public Vector3Int WorldPosition;
+	public Vector3Int BoundingLower;
+	public Vector3Int BoundingUpper;
+	public Vector2Int Box2DLower;
+	public Vector2Int Box2DUpper;
+	public Vector3Int Angles;
 	public int LastOffset;
 	public float LastDistance;
 	public int HotBoxSize;
-	public Vector3 HotPosition;
+	public Vector3Int HotPosition;
 	public Box BoxHotPoint;
 	public Box BoxWorldPos;
 	public int OldAngle;
@@ -61,34 +61,34 @@ public class Box : MonoBehaviour
 	public int HitBy;
 	public int Hit;
 	public int ColBy;
-	public Vector3 Col;
+	public Vector3Int Col;
 	public int HardCol;
 	public int HardTrigger;
 	public DosBox DosBox;
-	public Vector3 CameraPosition;
-	public Vector3 CameraRotation;
-	public Vector3 CameraFocal;
+	public Vector3Int CameraPosition;
+	public Vector3Int CameraRotation;
+	public Vector3Int CameraFocal;
 
-	public Vector3 BoundingPos
+	public Vector3Int BoundingPos
 	{
 		get
 		{
-			return new Vector3(
-				(int)((BoundingUpper.x + BoundingLower.x)) / 2,
-				(int)((BoundingUpper.y + BoundingLower.y)) / 2,
-				(int)((BoundingUpper.z + BoundingLower.z)) / 2
+			return new Vector3Int(
+				(BoundingUpper.x + BoundingLower.x) / 2,
+				(BoundingUpper.y + BoundingLower.y) / 2,
+				(BoundingUpper.z + BoundingLower.z) / 2
 			);
 		}
 	}
 
-	public Vector3 BoundingSize
+	public Vector3Int BoundingSize
 	{
 		get
 		{
-			return new Vector3(
-				(BoundingUpper.x - BoundingLower.x),
-				(BoundingUpper.y - BoundingLower.y),
-				(BoundingUpper.z - BoundingLower.z)
+			return new Vector3Int(
+				BoundingUpper.x - BoundingLower.x,
+				BoundingUpper.y - BoundingLower.y,
+				BoundingUpper.z - BoundingLower.z
 			);
 		}
 	}
@@ -247,9 +247,9 @@ public class Box : MonoBehaviour
 
 		if (name == "Camera" && DosBox.ShowAdditionalInfo)
 		{
-			Vector3 position = CameraPosition * 10.0f;
-			Vector3 rotation = CameraRotation * (360.0f / 1024.0f);
-			info.Append("POSITION", "{0:F0} {1:F0} {2:F0}", position.x, -position.y, -position.z);
+			Vector3Int position = CameraPosition * 10;
+			Vector3 rotation = (Vector3)CameraRotation * (360.0f / 1024.0f);
+			info.Append("POSITION", "{0} {1} {2}", position.x, -position.y, -position.z);
 			info.Append("ANGLE", "{0:N1} {1:N1} {2:N1}",
 				rotation.x > 180.0f ? (rotation.x - 360.0f) : rotation.x,
 				(-rotation.y + 540.0f) % 360.0f,

Assets/Scripts/Room/BoxInfo.cs
@@ -31,7 +31,7 @@ public class BoxInfo : MonoBehaviour
 		AppendLineIfNecessary();
 	}
 
-	public void Append(string name, Vector3 value)
+	public void Append(string name, Vector3Int value)
 	{
 		AppendLineIfNecessary();
 		names.Append(name);

Assets/Scripts/Room/CameraHelper.cs
@@ -22,14 +22,14 @@ public class CameraHelper : MonoBehaviour
 		9, 10, 11, 8            //parallel lines behind
 	};
 
-	public void SetupTransform(Transform transform, Vector3 cameraPosition)
+	public void SetupTransform(Transform transform, Vector3Int cameraPosition)
 	{
 		transform.position = new Vector3(cameraPosition.x, cameraPosition.y, -cameraPosition.z) / 100.0f;
 	}
 
-	public Mesh CreateMesh(Vector3 cameraRotation, Vector3 cameraFocal)
+	public Mesh CreateMesh(Vector3Int cameraRotation, Vector3Int cameraFocal)
 	{
-		var qrot = Quaternion.Euler(cameraRotation / 1024.0f * 360.0f);
+		var qrot = Quaternion.Euler((Vector3)cameraRotation / 1024.0f * 360.0f);
 		var offset = qrot * new Vector3(0.0f, 0.0f, cameraFocal.x) / 1000.0f;
 		var focal = new Vector3(160.0f / cameraFocal.y, 1.0f, 100.0f / cameraFocal.z);
 

Assets/Scripts/Room/RoomLoader.cs
@@ -234,7 +234,7 @@ public class RoomLoader : MonoBehaviour
 		roomObject.transform.parent = transform;
 		rooms.Add(roomObject.transform);
 
-		Vector3 roomPosition = buffer.ReadVector(roomheader + 4);
+		Vector3Int roomPosition = buffer.ReadVector(roomheader + 4);
 		roomObject.transform.localPosition = new Vector3(roomPosition.x, roomPosition.y, -roomPosition.z) / 100.0f;
 
 		//colliders
@@ -249,12 +249,12 @@ public class RoomLoader : MonoBehaviour
 			box.Room = currentroom;
 			box.transform.parent = roomObject.transform;
 
-			Vector3 lower, upper;
+			Vector3Int lower, upper;
 			buffer.ReadBoundingBox(i + 0, out lower, out upper);
 
-			Vector3 position = lower + upper;
+			Vector3Int position = lower + upper;
 			box.transform.localPosition = new Vector3(position.x, -position.y, position.z) / 2000.0f;
-			box.transform.localScale = (upper - lower) / 1000.0f;
+			box.transform.localScale = (Vector3)(upper - lower) / 1000.0f;
 			box.transform.localScale = Vector3.Max(box.transform.localScale, Vector3.one * 0.01f);
 
 			box.ID = buffer.ReadShort(i + 12);
@@ -292,12 +292,12 @@ public class RoomLoader : MonoBehaviour
 			box.Room = currentroom;
 			box.transform.parent = roomObject.transform;
 
-			Vector3 lower, upper;
+			Vector3Int lower, upper;
 			buffer.ReadBoundingBox(i + 0, out lower, out upper);
 
-			Vector3 position = lower + upper;
+			Vector3Int position = lower + upper;
 			box.transform.localPosition = new Vector3(position.x, -position.y, position.z) / 2000.0f;
-			box.transform.localScale = (upper - lower) / 1000.0f;
+			box.transform.localScale = (Vector3)(upper - lower) / 1000.0f;
 
 			box.ID = buffer.ReadShort(i + 12);
 			box.Flags = buffer.ReadShort(i + 14);

commit 4374031aa3a005fd7f0e7ef22a685e7bdcd374d8

Fix bug related to bounding box introduced in fa55ac2

Assets/Scripts/Model/ModelLoader.cs
@@ -98,7 +98,7 @@ public class ModelLoader : MonoBehaviour
 		//bounding box
 		buffer.ReadBoundingBox(i + 2, out boundingLower, out boundingUpper);
 		BoundingBox.transform.localScale = (Vector3)(boundingUpper - boundingLower) / 1000.0f;
-		Vector3Int pos = (boundingUpper + boundingLower) / 2000;
+		Vector3 pos = (Vector3)(boundingUpper + boundingLower) / 2000.0f;
 		BoundingBox.transform.localPosition = new Vector3(pos.x, -pos.y, pos.z);
 
 		i += 0xE;