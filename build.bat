8770448a0e0b146f8a62c0bdc2dc018833e3ef26
Tigrou Ind <tigrou.ind@gmail.com>
2021-11-22 21:17:28 +0100

    Add build.bat file

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+
+"%ProgramFiles%\Unity 5.5.4p3\Editor\unity.exe" -batchmode -builtTarget win64 -buildWindows64Player "%TEMP%\AITD room viewer.exe"  -quit
+if %ERRORLEVEL% NEQ 0 pause
+
+"%PROGRAMFILES%\7-Zip\7z" a -tzip "AITD_room_viewer.zip" ^
+ "%TEMP%\AITD room viewer_Data" ^
+ "%TEMP%\AITD room viewer.exe" ^
+ "-mx=9"
+if %ERRORLEVEL% NEQ 0 pause
+
+rd /s/q "AITD room viewer_Data"
+del "AITD room viewer.exe"
