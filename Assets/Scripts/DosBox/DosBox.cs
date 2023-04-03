tigrouind/AITD-roomviewer

commit 050990c8f6eb186b76c5bdc9d702de405d971201

Refactor

Assets/Scripts/DosBox/DosBox.cs
@@ -247,7 +247,7 @@ public class DosBox : MonoBehaviour
 				if (GetComponent<RoomLoader>().TryGetRoomPosition(box.Floor, box.Room, out roomPosition))
 				{
 					//local to global position
-					Vector3 boxPosition = (Vector3)(box.BoundingPos + roomPosition) / 1000.0f;
+					Vector3 boxPosition = (Vector3)(box.BoundingUpper + box.BoundingLower) / 2000.0f + (Vector3)roomPosition / 1000.0f;
 					boxPosition = new Vector3(boxPosition.x, -boxPosition.y, boxPosition.z);
 
 					if (box.transform.position != boxPosition)
@@ -464,7 +464,7 @@ public class DosBox : MonoBehaviour
 				}
 
 				Vector3 finalPos = (Vector3)(box.WorldPosition + box.Mod + currentRoomPos) / 1000.0f;
-				float height = -box.BoundingPos.y / 1000.0f;
+				float height = -(box.BoundingUpper.y + box.BoundingUpper.y) / 2000.0f;
 				finalPos = new Vector3(finalPos.x, height + 0.001f, finalPos.z);
 				worldPos.transform.position = finalPos;
 				worldPos.transform.localScale = box.transform.localScale;

Assets/Scripts/Room/Box.cs
@@ -146,6 +146,15 @@ public class Box : MonoBehaviour
 		}
 	}
 
+	public void SetPositionAndSize()
+	{
+		var position = (Vector3)(BoundingUpper + BoundingLower) / 2000.0f;		
+		var scale = (Vector3)BoundingSize / 1000.0f;
+
+		transform.localPosition = new Vector3(position.x, -position.y, position.z);		
+		transform.localScale = Vector3.Max(scale, Vector3.one * 0.01f);
+	}
+
 	string RotateDir
 	{
 		get

Assets/Scripts/Room/RoomLoader.cs
@@ -256,13 +256,8 @@ public class RoomLoader : MonoBehaviour
 			box.Room = currentroom;
 			box.transform.parent = roomObject.transform;
 
-			Vector3Int lower, upper;
-			buffer.ReadBoundingBox(i + 0, out lower, out upper);
-
-			Vector3Int position = lower + upper;
-			box.transform.localPosition = new Vector3(position.x, -position.y, position.z) / 2000.0f;
-			box.transform.localScale = (Vector3)(upper - lower) / 1000.0f;
-			box.transform.localScale = Vector3.Max(box.transform.localScale, Vector3.one * 0.01f);
+			buffer.ReadBoundingBox(i + 0, out box.BoundingLower, out box.BoundingUpper);
+			box.SetPositionAndSize();
 
 			box.ID = buffer.ReadShort(i + 12);
 			box.Flags = buffer.ReadShort(i + 14);
@@ -304,12 +299,8 @@ public class RoomLoader : MonoBehaviour
 			box.Room = currentroom;
 			box.transform.parent = roomObject.transform;
 
-			Vector3Int lower, upper;
-			buffer.ReadBoundingBox(i + 0, out lower, out upper);
-
-			Vector3Int position = lower + upper;
-			box.transform.localPosition = new Vector3(position.x, -position.y, position.z) / 2000.0f;
-			box.transform.localScale = (Vector3)(upper - lower) / 1000.0f;
+			buffer.ReadBoundingBox(i + 0, out box.BoundingLower, out box.BoundingUpper);
+			box.SetPositionAndSize();
 
 			box.ID = buffer.ReadShort(i + 12);
 			box.Flags = buffer.ReadShort(i + 14);

commit 0c1bcfdf7f10b6c1b7f0559c3e2c1ecbf1839bf0

Fix bug related to player unsync box (introduced in 050990c8f6)

Assets/Scripts/DosBox/DosBox.cs
@@ -494,7 +494,7 @@ public class DosBox : MonoBehaviour
 				}
 
 				Vector3 finalPos = (Vector3)(box.WorldPosition + box.Mod + currentRoomPos) / 1000.0f;
-				float height = -(box.BoundingUpper.y + box.BoundingUpper.y) / 2000.0f;
+				float height = -(box.BoundingUpper.y + box.BoundingLower.y) / 2000.0f;
 				finalPos = new Vector3(finalPos.x, height + 0.001f, finalPos.z);
 				worldPos.transform.position = finalPos;
 				worldPos.transform.localScale = box.transform.localScale;