d96e45eb268c1095e9cb279bbcd634527db0df13
Tigrou <tigrou.ind@gmail.com>
2025-11-13 14:45:19 +0100

    Increase EXE size for entry point detection.

--- Assets/Scripts/DosBox/DosBoxSearch.cs
+++ Assets/Scripts/DosBox/DosBoxSearch.cs
@@ -65,7 +65,7 @@ public static class DosBoxSearch
 	public static bool TryGetExeEntryPoint(byte[] memory, out int entryPoint)
 	{
 		int psp = DosMCB.GetMCBs(memory)
-			.Where(x => x.Size > 100 * 1024 && x.Size < 200 * 1024 && x.Owner != 0) //is AITD exe loaded yet?
+			.Where(x => x.Size > 100 * 1024 && x.Size < 250 * 1024 && x.Owner != 0) //is AITD exe loaded yet?
 			.Select(x => x.Owner)
 			.FirstOrDefault();
 
